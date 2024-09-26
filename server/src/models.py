962ca28f12e260617780215820751fc31c0913e0
ben mccarthy <bmccarthy90@gmail.com>
2024-09-26 15:55:04 -0400

    added client and server folders, started on app.py and models.py

---
+++ server/src/models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from flask_bcrypt import Bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+
+# metadata to fix alembic bug
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+# init sqlalchemy object
+db = SQLAlchemy(metadata=MetaData(naming_convention=convention))
+
+# init bcrypt plugin
+bcrypt = Bcrypt()
